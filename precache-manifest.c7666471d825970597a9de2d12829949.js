1f25f087b347d03ed2dee1602ce76c7f53e26ae1
user.name <git config --global user>
2020-06-02 20:16:25 +0530

    Updates

---
+++ precache-manifest.c7666471d825970597a9de2d12829949.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e8cc528d170a9a835d0cf50b0ef90ab",
+    "url": "/elecalwaytobank/index.html"
+  },
+  {
+    "revision": "79190b4f701b01790d2d",
+    "url": "/elecalwaytobank/static/css/2.d811125f.chunk.css"
+  },
+  {
+    "revision": "71e7332b4124b53fd008",
+    "url": "/elecalwaytobank/static/css/main.97d01ff8.chunk.css"
+  },
+  {
+    "revision": "79190b4f701b01790d2d",
+    "url": "/elecalwaytobank/static/js/2.0b626196.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/elecalwaytobank/static/js/2.0b626196.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71e7332b4124b53fd008",
+    "url": "/elecalwaytobank/static/js/main.8dc8e8b5.chunk.js"
+  },
+  {
+    "revision": "c9eb39b4e51a13e5aa11",
+    "url": "/elecalwaytobank/static/js/runtime-main.2849c968.js"
+  }
+]);
